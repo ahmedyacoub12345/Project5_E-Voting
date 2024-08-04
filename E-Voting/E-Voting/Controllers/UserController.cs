c6714e857a52c51e046af9e632851c183adee44c
ahmed-yacoub <ahmedyacoub718@gmail.com>
2024-08-04 09:15:43 +0300

    Project5

--- E-Voting/E-Voting/Controllers/UserController.cs
+++ E-Voting/E-Voting/Controllers/UserController.cs
@@ -6,8 +6,6 @@ using System.Net;
 using System.Text;
 using System.Web.Mvc;
 using Newtonsoft.Json;
-using System.Xml;
-using System.Threading;
 
 namespace E_Voting.Controllers
 {
@@ -21,7 +19,7 @@ namespace E_Voting.Controllers
             return View();
         }
 
-
+        // الأكشن لتسجيل الدخول
         public ActionResult Login(User user)
         {
             try
@@ -49,13 +47,11 @@ namespace E_Voting.Controllers
                     SendConfirmationEmail(existingUser.Email, newPassword);
 
                     ViewBag.Emailsent = "The code has been sent to your Email";
-                    return RedirectToAction("LoginUser", new { NationalNumber = user.NationalNumber });
                 }
-                else
-                {
-                    ViewBag.NationalId = existingUser.NationalNumber;
-                    return RedirectToAction("LoginUser", new { NationalNumber = user.NationalNumber });
-                }
+
+                // تخزين المستخدم في الجلسة وإعادة التوجيه إلى LoginUser
+                Session["LoggedUser"] = JsonConvert.SerializeObject(existingUser);
+                return RedirectToAction("LoginUser", new { ID =user.ID });
             }
             catch (Exception ex)
             {
@@ -66,9 +62,10 @@ namespace E_Voting.Controllers
             return View();
         }
 
-        public ActionResult LoginUser(string NationalNumber, string Email, string password)
+        // الأكشن لاستقبال طلب تسجيل الدخول
+        public ActionResult LoginUser(string nationalNumber)
         {
-            var user = DB.Users.FirstOrDefault(u => u.NationalNumber == NationalNumber);
+            var user = DB.Users.FirstOrDefault(u => u.NationalNumber == nationalNumber);
 
             if (user == null)
             {
@@ -76,20 +73,15 @@ namespace E_Voting.Controllers
                 return View();
             }
 
-            if (ModelState.IsValid)
-            {
-                if (Email == user.Email && password == user.Password)
-                {
-                    TempData["LoggedUser"] = JsonConvert.SerializeObject(user);
-                    return RedirectToAction("TypeOfElection", new { id = user.ID });
-                }
+            // تخزين المستخدم في الجلسة
+            Session["LoggedUser"] = JsonConvert.SerializeObject(user);
+            ViewBag.NationalNumber = nationalNumber;
+            ViewBag.Email = user.Email;
+            return RedirectToAction("TypeOfElection", new { id = user.ID});
 
-                ModelState.AddModelError("", "Invalid login attempt.");
-            }
-
-            return View();
         }
 
+        // توليد كلمة مرور عشوائية
         private string GenerateRandomPassword()
         {
             const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
@@ -103,7 +95,7 @@ namespace E_Voting.Controllers
             return res.ToString();
         }
 
-        // Sends a confirmation email with the new password
+        // إرسال بريد تأكيد بكلمة المرور الجديدة
         private void SendConfirmationEmail(string toEmail, string confirmationCode)
         {
             string fromEmail = System.Configuration.ConfigurationManager.AppSettings["FromEmail"];
@@ -134,6 +126,8 @@ namespace E_Voting.Controllers
                 }
             }
         }
+
+        // الأكشن لتحديد نوع الانتخابات
         public ActionResult TypeOfElection(int id)
         {
             var user = DB.Users.Find(id);
@@ -142,57 +136,119 @@ namespace E_Voting.Controllers
                 return HttpNotFound();
             }
 
-            //LocalElection
-            if (user.LocalElections is false)
+            // تحديد مسارات الانتخابات المحلية
+            if ((bool)!user.LocalElections)
             {
-
                 ViewBag.LocalElectionsPath = "LocalElections";
             }
             else
             {
                 ViewBag.LocalElectionsPath = null;
             }
-            //WhiteLocalElection
-            if (user.whitePaperLocalElections is false)
-            {
 
-                ViewBag.LocalElectionsPath = "White LocalElections";
+            // تحديد مسارات الانتخابات المحلية بالورقة البيضاء
+            if ((bool)!user.whitePaperLocalElections)
+            {
+                ViewBag.WhiteLocalElectionsPath = "White LocalElections";
             }
             else
             {
-                ViewBag.LocalElectionsPath = null;
+                ViewBag.WhiteLocalElectionsPath = null;
             }
 
-            //PartyElections
-            if (user.PartyElections is false)
+            // تحديد مسارات انتخابات الحزب
+            if ((bool)!user.PartyElections)
             {
-
                 ViewBag.PartyElections = "PartyElections";
             }
             else
             {
                 ViewBag.PartyElections = null;
-
             }
 
             return View();
         }
 
+        // الأكشن للانتخابات المحلية
         public ActionResult LocalElections()
         {
-            var userJson = TempData["LoggedUser"].ToString();
+            if (Session["LoggedUser"] == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var userJson = Session["LoggedUser"].ToString();
             var user = JsonConvert.DeserializeObject<User>(userJson);
 
+            ViewBag.UserId = user.ID;
 
-            return View();
+            var localLists = DB.LocalLists.ToList();
+            return View(localLists);
         }
+
+        [HttpPost]
+        public ActionResult LocalElections(int selectedListId)
+        {
+            if (Session["LoggedUser"] == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var userJson = Session["LoggedUser"].ToString();
+            var user = JsonConvert.DeserializeObject<User>(userJson);
+
+            var selectedList = DB.LocalLists.Find(selectedListId);
+            if (selectedList != null)
+            {
+                // Update user table to reflect that the user has voted in local elections
+                user.LocalElections = true;
+                DB.Entry(user).State = System.Data.Entity.EntityState.Modified;
+                DB.SaveChanges();
+            }
+
+            return RedirectToAction("TypeOfElection", new { id = user.ID });
+        }
+
+
         public ActionResult PartyElections()
         {
-            var userJson = TempData["LoggedUser"].ToString();
-            var user = JsonConvert.DeserializeObject<User>(userJson);
-            var party = DB.GeneralListings.ToList();
-            return View();
-        }
-    }
+            if (Session["LoggedUser"] == null)
+            {
+                return RedirectToAction("Login");
+            }
 
+            var userJson = Session["LoggedUser"].ToString();
+            var user = JsonConvert.DeserializeObject<User>(userJson);
+
+            ViewBag.UserId = user.ID;
+
+            var partyLists = DB.GeneralListings.ToList();
+            return View(partyLists);
+        }
+
+        [HttpPost]
+        public ActionResult PartyElections(int selectedPartyListId)
+        {
+            if (Session["LoggedUser"] == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var userJson = Session["LoggedUser"].ToString();
+            var user = JsonConvert.DeserializeObject<User>(userJson);
+
+            var selectedPartyList = DB.GeneralListings.Find(selectedPartyListId);
+            if (selectedPartyList != null)
+            {
+                // Update user table to reflect that the user has voted in party elections
+                user.PartyElections = true;
+                DB.Entry(user).State = System.Data.Entity.EntityState.Modified;
+                DB.SaveChanges();
+            }
+
+            return RedirectToAction("TypeOfElection", new { id = user.ID });
+        }
+
+
+    }
 }
